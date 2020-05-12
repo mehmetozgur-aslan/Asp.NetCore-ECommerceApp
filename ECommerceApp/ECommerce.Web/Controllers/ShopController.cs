72ea41b886cb0403f4681b27945030db5ac8518e
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-05-12 23:22:38 +0300

    urun listeleme

--- ECommerceApp/ECommerce.Web/Controllers/ShopController.cs
+++ ECommerceApp/ECommerce.Web/Controllers/ShopController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Business.Abstract;
+using ECommerce.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Web.Controllers
 {
     public class ShopController : Controller
     {
+        private IProductService _productService;
+        public ShopController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(new ProductListModel()
+            {
+                Products = _productService.GetAll()
+            });
         }
     }
 }
